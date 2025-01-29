fd27fed7c7992348ea24603765fa64a9071a315f
Kerolos-devpro <kerolos.devpro@gmail.com>
2025-01-29 03:02:06 -0800

    sdds

---
+++ Utility/SD.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Utility
+{
+    public class SD
+    {
+        public const string Admin = "Admin";
+        public const string Customer = "Customer";
+        public const string Company = "Company";
+        public const string AdminEmail = "Ahmedzakria998@gmail.com";
+    }
+}
